538b04b190c3e2cca57710438e9e200ecd38c957
jos84mar20 <josemarulanda64@gmail.com>
2022-03-30 11:40:12 -0500

    inyecte el servicio y se creo la función para crear el usuario

--- src/app/home/list-user/add-user/add-user.component.ts
+++ src/app/home/list-user/add-user/add-user.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { Users } from 'src/app/interfaces/list.item.types';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-add-user',
@@ -7,35 +10,43 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./add-user.component.scss']
 })
 export class AddUserComponent implements OnInit {
-  @Input() title:string = ''
+  @Input() title: string = ''
   user: FormGroup;
 
-  public show =false;
-  constructor() { }
-createForm(){
-  this.user=new FormGroup({
-    email:new FormControl("",[Validators.required, Validators.email]),
-    name:new FormControl("",Validators.required),
-    username:new FormControl("",Validators.required),
-    phone:new FormControl("",Validators.required),
-
-  })
-}
+  public show = false;
+  constructor(private userServices: UsersService) { }
+  createForm() {
+    this.user = new FormGroup({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      name: new FormControl("", Validators.required),
+      username: new FormControl("", Validators.required),
+      phone: new FormControl("", Validators.required)
+    })
+  }
   ngOnInit(): void {
     this.createForm();
   }
-showModel(){
-  this.show=true;
-}
-  hideModel(){
-    this.show=false;
+  showModel() {
+    this.show = true;
   }
-  createUser(){
+  hideModel() {
+    this.show = false;
+  }
+  createUser() {
     console.log(this.user)
+    const newUser: Users = {
+      email: this.user.get("email")?.value,
+      name: this.user.get("name")?.value,
+      username: this.user.get("username")?.value,
+      phone: this.user.get('phone')?.value
+    };
+    this.userServices.saveUSer(newUser).subscribe(data =>
+      console.log('Información enviada'));
+    this.exit()
   }
-exit(){
-  this.user.reset()
-  this.show=false;
- 
-}
+  exit() {
+    this.user.reset()
+    this.show = false;
+
+  }
 }
